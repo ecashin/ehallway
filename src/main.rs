ac5ceccb0855b0b9073df336d32a565c7d7ccc5e
Ed Cashin <ed.cashin@acm.org>
2022-04-12 20:31:29 -0400

    Switch to rocket

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,12 @@
-use anyhow::Result;
-use oxhttp::model::{Response, Status};
-use oxhttp::Server;
-use std::time::Duration;
+#[macro_use]
+extern crate rocket;
 
-fn main() -> Result<()> {
-    // Builds a new server that returns a 404 everywhere except for "/"
-    // where it returns the body 'home'
-    let mut server = Server::new(|request| {
-        if request.url().path() == "/" {
-            Response::builder(Status::OK).with_body("home")
-        } else {
-            Response::builder(Status::NOT_FOUND).build()
-        }
-    });
-    server.set_global_timeout(Duration::from_secs(10));
-    server.listen(("localhost", 8080)).unwrap();
-    Ok(())
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
