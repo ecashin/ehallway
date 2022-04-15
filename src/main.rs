9cdf358e45222b84c90d46c68f7e87fdebdc6f9c
Ed Cashin <ed.cashin@acm.org>
2022-04-14 20:05:34 -0400

    Use TLS

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,33 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::fs::FileServer;
+use rocket::mtls::Certificate;
+
+#[get("/eg/<rest..>")]
+fn eg_rest(rest: std::path::PathBuf) -> String {
+    format!("rest of path:{:?}", rest)
+}
+
 #[get("/")]
-fn index() -> &'static str {
+fn mutual(cert: Certificate<'_>) -> String {
+    format!(
+        "Hello! Here's what we know: [{}] {}",
+        cert.serial(),
+        cert.subject()
+    )
+}
+
+#[get("/", rank = 2)]
+fn hello() -> &'static str {
     "Hello, world!"
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    // See `Rocket.toml` and `Cargo.toml` for TLS configuration.
+    // Run `./private/gen_certs.sh` to generate a CA and key pairs.
+    rocket::build()
+        .mount("/", routes![eg_rest, hello, mutual])
+        .mount("/static", FileServer::from("static/"))
 }
