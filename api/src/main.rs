edea004ab8506c65f5506f73a73cfee397802b47
Ed Cashin <ed.cashin@acm.org>
2022-04-21 20:45:52 -0400

    Remove extra subdirectory

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+use rocket_auth::User;
+
+#[get("/user")]
+fn user_info(user: User) -> String {
+    format!("{:?}", user)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![user_info])
 }
