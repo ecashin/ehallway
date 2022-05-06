0a11c0f21e824f56fd775d5f09029e25aa05e340
Ed Cashin <ed.cashin@acm.org>
2022-05-05 20:34:33 -0400

    Use tokio_postgres example from rocket_auth

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,41 +1,102 @@
-#[macro_use]
-extern crate rocket;
+use rocket::{form::*, get, post, response::Redirect, routes, State};
+use rocket_auth::{prelude::Error, *};
+use rocket_dyn_templates::Template;
+use serde_json::json;
 
-use anyhow::Result;
-use rocket::State;
-use rocket_auth::{User, Users};
-use tokio_postgres::NoTls;
-
-#[get("/user")]
-fn user_info(user: User) -> String {
-    "user info".to_owned()
+use std::*;
+use std::{convert::TryInto, result::Result};
+use tokio_postgres::{connect, Client};
+#[get("/login")]
+fn get_login() -> Template {
+    Template::render("login", json!({}))
 }
 
-#[get("/users")]
-fn see_users(users: &State<Users>) -> String {
-    "see users done".to_owned()
+#[post("/login", data = "<form>")]
+async fn post_login(auth: Auth<'_>, form: Form<Login>) -> Result<Redirect, Error> {
+    let result = auth.login(&form).await;
+    println!("login attempt: {:?}", result);
+    result?;
+    Ok(Redirect::to("/"))
 }
 
-#[rocket::main]
-async fn main() -> Result<()> {
-    println!("here");
-    let (db_client, conn) = tokio_postgres::connect("postgres://vhallway:vhallway@127.0.0.1/", NoTls).await?;
+#[get("/signup")]
+async fn get_signup() -> Template {
+    Template::render("signup", json!({}))
+}
+
+#[post("/signup", data = "<form>")]
+async fn post_signup(auth: Auth<'_>, form: Form<Signup>) -> Result<Redirect, Error> {
+    auth.signup(&form).await?;
+    auth.login(&form.into()).await?;
+
+    Ok(Redirect::to("/"))
+}
+
+#[get("/")]
+async fn index(user: Option<User>) -> Template {
+    Template::render("index", json!({ "user": user }))
+}
+
+#[get("/logout")]
+fn logout(auth: Auth<'_>) -> Result<Template, Error> {
+    auth.logout()?;
+    Ok(Template::render("logout", json!({})))
+}
+#[get("/delete")]
+async fn delete(auth: Auth<'_>) -> Result<Template, Error> {
+    auth.delete().await?;
+    Ok(Template::render("deleted", json!({})))
+}
+
+#[get("/show_all_users")]
+async fn show_all_users(client: &State<sync::Arc<Client>>, user: Option<User>) -> Result<Template, Error> {
+    let users: Vec<User> = client
+        .query("select * from users;", &[])
+        .await?
+        .into_iter()
+        .map(TryInto::try_into)
+        .flatten()
+        .collect();
+
+    Ok(Template::render(
+        "users",
+        json!({"users": users, "user": user}),
+    ))
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    use tokio_postgres::NoTls;
+    let (client, conn) = connect("host=localhost user=vhallway password=vhallway", NoTls).await?;
+    let client = sync::Arc::new(client);
+    let users: Users = client.clone().into();
+    
+
     tokio::spawn(async move {
         if let Err(e) = conn.await {
-            eprintln!("connection error: {}", e);
+            eprintln!("TokioPostgresError: {}", e);
         }
     });
-    println!("there");
-    let rows = db_client
-        .query("SELECT $1::TEXT", &[&"hello world"])
-        .await?;
-    let value: &str = rows[0].get(0);
-    assert_eq!(value, "hello world");
-    println!("everywhere");
-
-    let users: Users = db_client.into();
-    users.create_table().await?;
-
-    rocket::build().mount("/", routes![see_users, user_info]).manage(db_client).manage(users).launch().await.expect("launching");
+    users.create_table().await?; 
+    rocket::build()
+        .mount(
+            "/",
+            routes![
+                index,
+                get_login,
+                post_signup,
+                get_signup,
+                post_login,
+                logout,
+                delete,
+                show_all_users
+            ],
+        )
+        .manage(client)
+        .manage(users)
+        .attach(Template::fairing())
+        .launch()
+        .await
+        .unwrap();
     Ok(())
 }
