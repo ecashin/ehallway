f059e3ce5930fce46f555ee0901c15d2cc1c5102
Ed Cashin <ed.cashin@acm.org>
2022-05-13 18:16:56 -0400

    Build tokio runtime for doing AJAX

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -9,6 +9,24 @@ struct Model {
     value: Option<i32>,
 }
 
+fn inc_and_fetch() -> i32 {
+    let runtime = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+    let msg: UserValueMessage = runtime.block_on(async {
+        reqwest::Client::new()
+            .get("https://localhost/inc")
+            .send()
+            .await
+            .unwrap()
+            .json()
+            .await
+            .unwrap()
+    });
+    msg.metric
+}
+
 #[derive(Clone, Deserialize, PartialEq)]
 struct UserValueMessage {
     metric: i32,
@@ -25,11 +43,7 @@ impl Component for Model {
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
             Msg::AddOne => {
-                let msg: UserValueMessage = reqwest::blocking::get("https://localhost/inc")
-                    .unwrap()
-                    .json()
-                    .unwrap();
-                self.value = Some(msg.metric);
+                self.value = Some(inc_and_fetch());
                 true
             }
         }
