c77a8c366874fa814da3becdf177f510b6cae887
Ed Cashin <ed.cashin@acm.org>
2022-05-21 19:42:00 -0400

    Implement Borda count method

--- ui/src/cull.rs
+++ ui/src/cull.rs
@@ -1,4 +1,5 @@
 // It would be nice to use tallystick, but I don't want to use nightly.
+use anyhow::{anyhow, Result};
 
 pub struct Ranking {
     // Entries are ordered to correspond to an array of choices.
@@ -13,14 +14,31 @@ fn argsort(a: &[usize]) -> Vec<usize> {
     indexed.iter().map(|(i, _v)| *i).collect::<Vec<usize>>()
 }
 
-pub fn borda_count(rankings: &[Ranking]) -> Vec<usize> {
+pub fn borda_count(rankings: &[Ranking]) -> Result<Vec<usize>> {
+    if rankings.is_empty() {
+        return Ok(vec![]);
+    }
+    let off0 = &rankings[..rankings.len() - 1];
+    let off1 = &rankings[1..];
+    let sum_diffs = off0
+        .iter()
+        .zip(off1.iter())
+        .map(|(a, b)| (a.scores.len() as isize - b.scores.len() as isize).pow(2))
+        .sum::<isize>();
+    if sum_diffs != 0 {
+        return Err(anyhow!("lengths of rankings differ"));
+    }
     let rankings: Vec<_> = rankings.iter().map(|r| argsort(&r.scores)).collect();
-    todo!()
+    let mut scores: Vec<_> = vec![];
+    for i in 0..rankings[0].len() {
+        scores.push((0..rankings.len()).map(|j| rankings[j][i]).sum());
+    }
+    Ok(scores)
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{argsort, borda_count};
+    use super::{argsort, borda_count, Ranking};
 
     #[test]
     fn test_argsort() {
@@ -32,5 +50,39 @@ mod tests {
         for (i, j) in a.iter().zip(bb.iter()) {
             assert_eq!(i, j);
         }
+    }
+
+    #[test]
+    fn test_borda_count_agree() {
+        let rankings = [
+            Ranking {
+                scores: vec![0, 1, 2],
+            },
+            Ranking {
+                scores: vec![3, 4, 5],
+            },
+            Ranking {
+                scores: vec![6, 7, 8],
+            },
+        ];
+        let count = borda_count(&rankings).unwrap();
+        assert_eq!(count, [0, 3, 6]);
+    }
+
+    #[test]
+    fn test_borda_count_disagree() {
+        let rankings = [
+            Ranking {
+                scores: vec![0, 1, 2],
+            },
+            Ranking {
+                scores: vec![3, 4, 5],
+            },
+            Ranking {
+                scores: vec![8, 7, 6],
+            },
+        ];
+        let count = borda_count(&rankings).unwrap();
+        assert_eq!(count, [2, 3, 4]);
     }
 }
