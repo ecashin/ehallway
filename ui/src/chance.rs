d9db5d65c6f43785a21961e7fb9612bd92d56a37
Ed Cashin <ed.cashin@acm.org>
2022-05-21 16:57:38 -0400

    Add cohort selection and test

--- ui/src/chance.rs
+++ ui/src/chance.rs
@@ -1,7 +1,20 @@
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Result};
+use rand::{seq::SliceRandom, thread_rng};
 
-pub fn cohorts(n_participants: usize, n_cohorts: usize) -> Result<()> {
-    todo!()
+pub fn cohorts(n_participants: usize, cohort_size: usize) -> Result<Vec<Vec<usize>>> {
+    if cohort_size > n_participants {
+        return Err(anyhow!(
+            "not enough participants ({}) for a cohort",
+            n_participants
+        ));
+    }
+    let rng = &mut thread_rng();
+    let mut order: Vec<usize> = (0..n_participants).collect();
+    order.shuffle(rng);
+    Ok(order
+        .chunks(cohort_size)
+        .map(|cohort| cohort.to_vec())
+        .collect::<Vec<_>>())
 }
 
 #[cfg(test)]
@@ -11,7 +24,14 @@ mod tests {
 
     #[test]
     fn test_cohorts() -> Result<()> {
-        cohorts(3, 1)?;
+        let mut c = cohorts(3, 1)?;
+        assert_eq!(c.len(), 3);
+        assert_eq!(c[0].len(), 1);
+        c = cohorts(3, 2)?;
+        println!("{:?}", c);
+        assert_eq!(c.len(), 2);
+        assert_eq!(c[0].len(), 2);
+        assert_eq!(c[1].len(), 1);
         Ok(())
     }
 }
